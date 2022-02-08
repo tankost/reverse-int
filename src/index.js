76349b3c87c07e85d428e0f10fda91dc3d32c079
Tatiana Kostukevich <tankostuk@gmail.com>
2022-02-09 01:52:30 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const arr = Math.abs(n).toString().split('');
+    let reverseArr = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reverseArr.push(arr[i]);
+    }
+    return reverseArr.join('');
 }
